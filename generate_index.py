dc0ef196de75255e3e12ec815e5a264aad9fbfc6
Nate Stemen <nate.stemen@overleaf.com>
2021-10-23 11:38:15 -0400

    remove unnecessary CLI options

--- generate_index.py
+++ generate_index.py
@@ -25,8 +25,6 @@ the filename can be explicitly specified
      will be used instead of common markdown
 
 author: elfnor <elfnor.com>
-credit : the code in get_headers comes from
-         https://github.com/amaiorano/md-to-toc
 """
 import re
 import os
@@ -45,52 +43,7 @@ def toggles_block_quote(line):
     return n_block_quote > 0 and line.count("```") % 2 != 0
 
 
-def get_headers(filename):
-    """code  from https://github.com/amaiorano/md-to-toc"""
-    in_block_quote = False
-    results = []  # list of (header level, title, anchor) tuples
-    last_line = ""
-
-    with open(filename) as file:
-        for line in file.readlines():
-
-            if toggles_block_quote(line):
-                in_block_quote = not in_block_quote
-
-            if in_block_quote:
-                continue
-
-            found_header = False
-            header_level = 0
-
-            match = HEADER_LINE_RE.match(line)
-            if match is not None:
-                header_level = len(match.group(1))
-                title = match.group(2)
-                found_header = True
-
-            if not found_header:
-                match = HEADER1_UNDERLINE_RE.match(line)
-                if match is not None:
-                    header_level = 1
-                    title = last_line.rstrip()
-                    found_header = True
-
-            if not found_header:
-                match = HEADER2_UNDERLINE_RE.match(line)
-                if match is not None:
-                    header_level = 2
-                    title = last_line.rstrip()
-                    found_header = True
-
-            if found_header:
-                results.append((header_level, title))
-
-            last_line = line
-    return results
-
-
-def create_index(cwd, headings=False, wikilinks=False):
+def create_index(cwd):
     """create markdown index of all markdown files in cwd and sub folders"""
     base_len = len(cwd)
     base_level = cwd.count(os.sep)
@@ -115,31 +68,11 @@ def create_index(cwd, headings=False, wikilinks=False):
             rel_dir = ".{1}{0}".format(os.sep, root[base_len:])
             for md_filename in files:
                 indent = "  " * level
-                if wikilinks:
-                    md_lines.append(
-                        "{0} {3} [[{2}{1}]]\n".format(
-                            indent,
-                            os.path.splitext(md_filename)[0],
-                            rel_dir,
-                            TOC_LIST_PREFIX,
-                        )
+                md_lines.append(
+                    "{0} {3} [{1}]({2}{1})\n".format(
+                        indent, md_filename, rel_dir, TOC_LIST_PREFIX
                     )
-                else:
-                    md_lines.append(
-                        "{0} {3} [{1}]({2}{1})\n".format(
-                            indent, md_filename, rel_dir, TOC_LIST_PREFIX
-                        )
-                    )
-                if headings:
-                    results = get_headers(os.path.join(root, md_filename))
-                    if len(results) > 0:
-                        min_header_level = min(results, key=lambda e: e[0])[0]
-                        for header in results:
-                            header_level = header[0] - min_header_level + level + 2
-                            indent = "  " * header_level
-                            md_lines.append(
-                                "{}{} {}\n".format(indent, TOC_LIST_PREFIX, header[1])
-                            )
+                )
 
     md_lines.append("\n<!-- filetreestop -->\n")
     return md_lines
@@ -188,20 +121,11 @@ def main():
     parser.add_argument(
         "filename", nargs="?", default="index.md", help="markdown output file"
     )
-    parser.add_argument(
-        "--full", "-f", action="store_true", help="list headers in files"
-    )
-    parser.add_argument(
-        "--wikilinks",
-        "-w",
-        action="store_true",
-        help="use [[link]] instead of [link.md](./link.md)",
-    )
 
     args = parser.parse_args()
 
     cwd = os.getcwd()
-    md_lines = create_index(cwd, headings=args.full, wikilinks=args.wikilinks)
+    md_lines = create_index(cwd)
 
     md_out_fn = os.path.join(cwd, args.filename)
     replace_index(md_out_fn, md_lines)
